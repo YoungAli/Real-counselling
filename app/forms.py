6922c6b7cd8182e38b4dbcb49fdd2653839b9e8e
Mannuel <oluwasegunprosperity@gmail.com>
2023-06-08 22:50:42 +0100

    update

--- app/forms.py
+++ app/forms.py
@@ -2,9 +2,10 @@ from django import forms
 from .models import Article, Appointment
 
 class ArticleForm(forms.ModelForm):
-    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': "Enter the article's title"}))
+    title = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': "Enter the article's title"}))
     content = forms.CharField(required=False, widget=forms.Textarea(attrs={'placeholder': "Start typing..."}))
     image_url = forms.CharField(required=False,widget=forms.TextInput(attrs={'placeholder': "Enter the image url"}))
+    tags = forms.CharField(required=False,widget=forms.TextInput(attrs={'placeholder': "Enter the tags separated with commas"}))
     class Meta:
         model = Article
         fields = ['title', 'tags', 'image_url', 'content']
@@ -12,15 +13,15 @@ class ArticleForm(forms.ModelForm):
 
 class AppointmentForm(forms.ModelForm):
     date = forms.DateField(
-        label="Select the date", required=False,
+        label="Select the date", required=True,
         widget=forms.widgets.DateInput(attrs={'type':'date'})
     )
     start_time = forms.TimeField(
-        label="Select the start time", required=False,
+        label="Select the start time", required=True,
         widget=forms.widgets.TimeInput(attrs={'type':'time'})
     )
     end_time = forms.TimeField(
-        label="Select the end time", required=False,
+        label="Select the end time", required=True,
         widget=forms.widgets.TimeInput(attrs={'type':'time'})
     )
 
