2c9c947b67c84ac9e348a9bd0982b335e67c87c5
Topman-14 <talktotimothy101@gmail.com>
2023-06-09 13:02:53 +0100

    update

--- static/js/articleslist.js
+++ static/js/articleslist.js
@@ -2,20 +2,31 @@ const searchBar = document.querySelector('#searchbar');
 const searchInput = document.querySelector('#search_input');
 const searchBtn = document.querySelector('#search_btn');
 const reader = document.querySelector('#article_reader');
-const GridWrapper = document.querySelector('#article_grid_wrapper');
+const gridWrapper = document.querySelector('#article_grid_wrapper');
+const mainWrapper = document.querySelector('#articles_main');
+const expandGrid = document.querySelector('#expand_grid');
+const articleCards = document.getElementsByClassName('card-article');
 let tagElements = document.getElementsByClassName('article_tags');
-function consolePop(text) {
+
+function consolePop(text, dur) {
     let popDiv = document.createElement('div');
     popDiv.id="console_msg_div"
     popDiv.innerHTML = `<div id="console_msg">${text}</div>`;
     document.body.prepend(popDiv);
     let popDivMsg = document.getElementById('console_msg');
     popDivMsg.style.animation = "pop-up 0.3s";
-    setTimeout(()=>{
-        popDivMsg.style.animation = "pop-down 0.3s";
-        setTimeout(()=>{document.getElementById('console_msg_div').style.display = 'none'}, 250)
-    }, 2000)
+    if(dur =! "infinite"){
+        setTimeout(()=>{
+            popDivMsg.style.animation = "pop-down 0.3s";
+            setTimeout(()=>{document.getElementById('console_msg_div').style.display = 'none'}, 250)
+        }, 2000)
+    }
 };
+
+if (!articleCards.length) {
+    console.log(articleCards.length);
+    consolePop(`<a href="{% url 'articles' %}">No search result, click to refresh articles</a>`, 'infinite')
+}
 
 for(let i=0; i< tagElements.length; i++){
     if(tagElements[i].textContent){
@@ -26,32 +37,49 @@ for(let i=0; i< tagElements.length; i++){
 }
 
 function openReader(){
-    GridWrapper.classList.add('show_reader');
-    reader.style.display = "unset";
+    if (window.innerWidth >= 800) {
+        mainWrapper.style.animation = "flicker 1s";
+        setTimeout(() => {
+            gridWrapper.classList.add('show_reader');
+            reader.style.display = "unset";
+            expandGrid.style.display = 'unset';
+        }, 500);
+        setTimeout(() => {mainWrapper.style.animation = "none"}, 1000);
+    }
+    
 }
 
 function closeReader(){
-    GridWrapper.classList.remove('show_reader');
-    reader.style.display = "none";
+    if (window.innerWidth >= 800){
+        mainWrapper.style.animation = "flicker 1s"
+        setTimeout(() => {
+            gridWrapper.classList.remove('show_reader');
+            reader.style.display = "none";
+            expandGrid.style.display = 'none';
+        }, 500);
+        setTimeout(() => {mainWrapper.style.animation = "none"}, 1000);
+    }
+    
 }
 
 function showArticle(title, tags, content, img){
-    openReader()
-    console.log(title)
-    console.log(tags)
-    console.log(tags.split(',').map(tag =>`<p>${tag}</p>`).join(" "))
-    console.log(img)
-    reader.innerHTML = `<h3 id="reader_title">${title}</h3> ${img? (`<img src="${img}" alt="${title}">`) : ""}
-    <div class="article_tags">
-        ${tags.split(',').map(tag =>`<p>${tag}</p>`).join(" ")}
-    </div>
-
-    <p id="reader_content">${content}</p>`;
+    openReader();
+    if (window.innerWidth >= 800){
+        setTimeout(() => {
+            reader.innerHTML = `<h3 id="reader_title">${title}</h3> ${img? (`<img src="${img}" alt="${title}">`) : ""}
+            <div class="article_tags">
+            ${tags.split(',').map(tag =>`<p>${tag}</p>`).join(" ")}
+            </div>
+            <p id="reader_content">${content}</p>`;
+        }, 500);
+    }
 }
 
 searchInput.addEventListener('keypress', (e)=>{
-    if (e.keyCode == 13)
-    searchBtn.click();
+    if (e.keyCode == 13){
+        searchBar.addEventListener('submit', (e)=>{e.preventDefault()})
+        searchBtn.click();
+    }
 })
 searchBtn.addEventListener('click', ()=>{
     if (!searchInput.value) {
@@ -60,6 +88,9 @@ searchBtn.addEventListener('click', ()=>{
     else{
         closeReader()
         searchBar.submit()
-        console.log('hello')
     }
+})
+
+expandGrid.addEventListener('click', ()=>{
+    closeReader();
 })
